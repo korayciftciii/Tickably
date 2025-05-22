8e36cbe438f013a9c801cf878f0ebbb88ea9c5c4
korayciftci <korayciftciii@users.noreply.github.com>
2025-05-23 01:06:13 +0300

    feat: add Convex client provider and sync user with Convex
    - Implemented ConvexClientProvider to manage Convex client state.
    - Created Header component with user authentication buttons and search functionality.
    - Developed SearchBar component for event searching.
    - Added SyncUserWithConvex component to synchronize user data with Convex.
    - Introduced reusable Button component with variant support.
    - Added utility function for class name merging.
    - Configured middleware for Clerk authentication.

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+export default {
+    providers: [
+        {
+            // domain: process.env.VITE_CLERK_FRONTEND_API_URL,
+            domain: "https://deep-basilisk-51.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexClientProvider } from "@/components/ConvexClientProvider";
+import Header from "@/components/Header";
+import SyncUserWithConvex from "@/components/SyncUserWithConvex";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +31,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ConvexClientProvider>
+          <ClerkProvider>
+            <Header />
+            <SyncUserWithConvex />
+            {children}
+          </ClerkProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   );
